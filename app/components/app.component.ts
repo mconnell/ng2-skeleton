f663974744366633eb38aec8870d30d3d36d31a5
Mark Connell <mark@markconnell.co.uk>
2016-01-07 12:02:33 +0000

    minimal angular app.

---
+++ app/components/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'skeleton',
+    template: `
+      <h1>Skeleton Application Works!</h1>
+    `
+})
+
+export class AppComponent {
+}
